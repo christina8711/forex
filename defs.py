0af95870ed3bdef0d466ff6d22f4e3bc5b0fc215
christina8711 <christina.nguyen8711@gmail.com>
2023-01-28 18:39:53 -0500

    Finally got it working

--- defs.py
+++ defs.py
@@ -1,5 +1,11 @@
-API_KEY = "xxxx"
-ACCOUNT_ID = "xxxx"
 
-OANDA_URL = "https://api-fxpractice.oanda.com/v3"
+API_KEY = "26f830fff9d69c2049992044ccf90d74-3c51923e4ede260abcaedbfccb7e3650"
+ACCOUNT_ID = "101-001-24655227-001"
+
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+
+}
 
